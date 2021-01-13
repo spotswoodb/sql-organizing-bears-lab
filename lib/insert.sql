c8e5fdad66be6c55fafc80e43ab628e38dd467a6
Ben Spotswood <spotswoodb@gmail.com>
2021-01-13 16:29:39 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, 'M', "black", "calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, 'M', "orange", "mad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, 'F', "orange", "calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, 'M', "black", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, 'F', "brown", "mad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, 'M', "black", "mad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, 'F', "brown", "calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', "brown", "mad", 1);
